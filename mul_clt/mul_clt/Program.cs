1ae0379324965aa4d5e8b4e69c184515f75b6b39
Lee-Jae-Ung <dlwodnd12345@gmail.com>
2022-01-02 16:56:06 +0900

    third commit

--- mul_clt/mul_clt/Program.cs
+++ mul_clt/mul_clt/Program.cs
@@ -1,12 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
 
-namespace mul_clt
+namespace client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program();
         }
+        public Program()
+        {
+            initialize();
+            Console.WriteLine("************Message**********");
+            Console.ReadLine();
+        }
+        private String m_Name;
+        public void initialize()
+        {
+            Console.Write("Name?");
+            String strName = Console.ReadLine();
+            if (strName.Trim().Length > 0)
+            {
+                m_Name = strName;
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(IPAddress.Parse("172.30.1.45"), 8280);
+                byte[] name = new byte[100];
+                byte[] buffer = Encoding.Unicode.GetBytes(strName);
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i < buffer.Length)
+                    {
+                        name[i] = buffer[i];
+                    }
+                }
+                client.Send(name);
+                SocketAsyncEventArgs reciveAsync = new SocketAsyncEventArgs();
+                reciveAsync.Completed += new EventHandler<SocketAsyncEventArgs>(reciveAsync_Completed);
+                reciveAsync.SetBuffer(new byte[4], 0, 4);
+                reciveAsync.UserToken = client;
+                client.ReceiveAsync(reciveAsync);
+            }
+        }
+        private void reciveAsync_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket client = (Socket)sender;
+            if (client.Connected && e.BytesTransferred > 0)
+            {
+                byte[] lengthByte = e.Buffer;
+                int length = BitConverter.ToInt32(lengthByte, 0);
+                byte[] data = new byte[length];
+                client.Receive(data, length, SocketFlags.None);
+                StringBuilder sb = new StringBuilder();
+                sb.Append(m_Name);
+                sb.Append(" - ");
+                sb.Append(Encoding.Unicode.GetString(data));
+
+                string rcv = Encoding.Unicode.GetString(data);
+
+                Console.WriteLine(sb.ToString());
+
+                int result = Int32.Parse(rcv);
+
+                if (result == 1)
+                {
+                    Type type = typeof(Reboot);
+                    MethodInfo method = type.GetMethod("Test");
+                    Reboot c = new Reboot();
+                    string result1 = (string)method.Invoke(c, null);
+                    Console.WriteLine(result1);
+                }
+
+                else if (result == 3)
+                {
+                    Type type = typeof(Reboot);
+                    MethodInfo method = type.GetMethod("Test");
+                    Reboot c = new Reboot();
+                    string result1 = (string)method.Invoke(c, null);
+                    Console.WriteLine(result1);
+                }
+            }
+            client.ReceiveAsync(e);
+        }
+    }
+}
+
+
+//출처: https://nowonbun.tistory.com/296 [명월 일지]
+
+class Reboot
+{
+    public static void Rebt()
+    {
+        System.Diagnostics.Process.Start("shutdow.exe", "-r");
+    }
+    public static void Test()
+    {
+        Console.Write("this is mac");
     }
 }
--- mul_sv/mul_sv/Program.cs
+++ mul_sv/mul_sv/Program.cs
@@ -1,12 +1,108 @@
 ﻿using System;
-
-namespace mul_sv
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+namespace server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program();
+        }
+        private Dictionary<string, Socket> m_client = null;
+        public Program()
+        {
+            initialize();
+        }
+        protected void initialize()
+        {
+            m_client = new Dictionary<string, Socket>();
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Any, 8280));
+            server.Listen(20);
+            SocketAsyncEventArgs sockAsync = new SocketAsyncEventArgs();
+            sockAsync.Completed += new EventHandler<SocketAsyncEventArgs>(sockAsync_Completed);
+            server.AcceptAsync(sockAsync);
+            Command();
+        }
+        private void sockAsync_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket server = (Socket)sender;
+            Socket client = e.AcceptSocket;
+            byte[] name = new byte[100];
+            client.Receive(name);
+            String strName = Encoding.Unicode.GetString(name).Trim().Replace("\0", "");
+            m_client.Add(strName, client);
+            SocketAsyncEventArgs recieveAsync = new SocketAsyncEventArgs();
+            recieveAsync.SetBuffer(new byte[4], 0, 4);
+            recieveAsync.UserToken = client;
+            recieveAsync.Completed += new EventHandler<SocketAsyncEventArgs>(recieveAsync_Completed);
+            client.ReceiveAsync(recieveAsync);
+            Console.WriteLine();
+            Console.WriteLine("***********" + strName + " Connected ***********");
+            Console.Write("Sender ?");
+            e.AcceptSocket = null;
+            server.AcceptAsync(e);
+        }
+        private void recieveAsync_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket client = (Socket)sender;
+            if (client.Connected && e.BytesTransferred > 0)
+            {
+                int length = BitConverter.ToInt32(e.Buffer, 0);
+                byte[] data = new byte[length];
+                client.Receive(data, length, SocketFlags.None);
+                String data2 = Encoding.Unicode.GetString(data);
+                String Name = searchSocket(client);
+                if (Name != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("***********" + Name + " receive ***********");
+                    Console.WriteLine(data2);
+                    Console.Write("Sender ?");
+                }
+                else
+                {
+                }
+            }
+            client.ReceiveAsync(e);
+        }
+        protected String searchSocket(Socket sender)
+        {
+            foreach (String key in m_client.Keys)
+            {
+                if (m_client[key] == sender)
+                {
+                    return key;
+                }
+            }
+            return null;
+        }
+        protected void Command()
+        {
+            while (true)
+            {
+                Console.Write("Sender ?");
+                String sender = Console.ReadLine();
+                if (m_client.ContainsKey(sender))
+                {
+                    Console.Write("Message ?");
+                    String message = Console.ReadLine();
+                    Socket client = m_client[sender];
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    client.Send(BitConverter.GetBytes(data.Length));
+                    client.Send(data, data.Length, SocketFlags.None);
+                }
+                else
+                {
+                    Console.WriteLine("Not Socket");
+                }
+            }
         }
     }
 }
+
+
+//출처: https://nowonbun.tistory.com/296 [명월 일지]asdasd
